76056c65b314ed570d7f8bcacfd4a4470c94f105
massively <stefan-hettich@live.de>
2015-07-08 17:51:21 +0200

    Added partly camera loading part. not finished with it.

--- where_is_waldo/src/controller.cpp
+++ where_is_waldo/src/controller.cpp
@@ -1,5 +1,8 @@
 #include "controller.h"
 
+using namespace std;
+using namespace base;
+
 Controller::Controller(int & argc, char ** argv)
 {
     gc = new GuiController(argc, argv);
@@ -52,6 +55,115 @@ void Controller::search_waldo(QList<QUrl> urls, TrainingData *data)
     c_class->test_classification(f->getFeature(1), f->getFeature(1));
 
     //TODO
+    /**
+     * the following part only work for a special set of picture.
+     * these are the pictures in the image dir of the project.
+     * these special pictures have been put in the program VisualFM
+     * and there a 3D scene of these picture was created.
+     * All positioning information of these pictures are saved in a
+     * .nvm-file which is also placed in the project (and is static).
+     *
+     * So to include other pictures it would be necessary to put them
+     * in VisualFM and create another 3D scene.
+     *
+     * VisualFM wasnt able to position all of our 38 pictures, so only
+     * 17 pictures were positioned and only in these 17 pictures, we
+     * are able to search for waldo.
+     *
+     * NOTE: Some of the following code is from intern projects of
+     * Prof. Dr. Lensch and his team and isnt public.
+     * So these files are not included in any public git repositories.
+     *
+     */
+
+
+}
+
+struct S {
+    char path[512];
+};
+
+vector<S> LoadFilenamesFromFile(const char* filename) {
+    vector<S> result;
+
+    ifstream fin(filename);
+    if (!fin.is_open())
+        exit(1);
+
+    string line;
+    getline(fin, line);
+
+    if (!line.compare("NVM_V3"))
+        exit(1);
+
+    getline(fin, line);
+    getline(fin, line);
+    unsigned numImages = atoi(line.c_str());
+    for (unsigned int i = 0; i < numImages; i++) {
+        getline(fin, line);
+
+        S s;
+
+        // only path is relevant.
+        float tmp;
+        sscanf(line.c_str(), "%s	%f %f %f %f %f %f %f %f", s.path, &tmp,
+                        &tmp, &tmp, &tmp, &tmp, &tmp, &tmp, &tmp);
+        // now save path
+
+        result.push_back(s);
+    }
+    fin.close();
+
+    return result;
+}
+
+vector<pair<CameraDataf, CameraPoseDataf> > LoadCamerasFromFile(
+        const char* filename)
+{
+    vector<pair<CameraDataf, CameraPoseDataf> > result;
+
+    ifstream fin(filename);
+    if (!fin.is_open())
+        exit(1);
+
+    string line;
+    getline(fin, line);
+
+    if (!line.compare("NVM_V3"))
+        exit(1);
+
+    getline(fin, line);
+    getline(fin, line);
+    unsigned numImages = atoi(line.c_str());
+    for (unsigned int i = 0; i < numImages; i++)
+    {
+        getline(fin, line);
+        char path[512];
+        RotationQuaternion<float> rotation;
+        Vec3f position;
+        float focalLength;
+        sscanf(line.c_str(), "%s	%f %f %f %f %f %f %f %f", path, &focalLength,
+                &rotation[3], &rotation[0], &rotation[2], &rotation[1],
+                &position[0], &position[2], &position[1]);
+
+        CameraDataf intrinsics;
+        intrinsics.FocalLength[0] = focalLength;
+        intrinsics.FocalLength[1] = focalLength;
+
+        QPixmap q(path);
+        //intrinsics.ImageSize = Vec2f(2848, 2136); // TODO: Set this correctly from image
+        intrinsics.ImageSize = Vec2f(q.width(), q.height());
+
+        CameraPoseDataf extrinsics;
+        extrinsics.Alignment = rotation.GetRotationMatrix();
+        for (unsigned int d = 0; d < 3; d++)
+            extrinsics.Alignment(d, 3) = position[d];
+
+        result.push_back(make_pair(intrinsics, extrinsics));
+    }
+    fin.close();
+
+    return result;
 }
 
 void Controller::load()
--- where_is_waldo/src/controller.h
+++ where_is_waldo/src/controller.h
@@ -1,11 +1,22 @@
 #ifndef CONTROLLER_H
 #define CONTROLLER_H
 
+#include <vector>
+#include <utility>
+#include <fstream>
+
+#include "camera_loading/CameraPoseData.h"
+#include "camera_loading/CameraData.h"
+#include "camera_loading/RotationQuaternion.hh"
+
 #include "data.h"
 #include "feature.h"
 #include "dataprovider.h"
 #include "../gui/guicontroller.h"
 #include "cpu_classifier.h"
+
+using namespace std;
+using namespace base;
 
 class Controller: public QObject
 {
@@ -14,6 +25,8 @@ class Controller: public QObject
 public:
     Controller(int & argc, char ** argv);
     int run();
+    vector<char[512]> LoadFilenamesFromFile(const char* filename);
+    vector<pair<CameraDataf, CameraPoseDataf> > LoadCamerasFromFile(const char* filename);
 
 private:
     GuiController *gc;
